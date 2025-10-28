5ad0c4d1388b6711c6306fe0b20c447e922c2fde
eltyreex <jofer.dipperfalls@gmail.com>
2025-10-28 12:44:34 -0600

    Add files via upload

---
+++ EVA2_23/src/eva2_23/EVA2_23.java
@@ -0,0 +1,9 @@
+package eva2_23;
+public class EVA2_23 {
+    public static void main(String[] args) {
+        while(true) { // Ciclo Infinito
+            System.out.println("Ciclo Infinito");}
+        System.out.print ("Bienvenido");
+    }
+    
+}
